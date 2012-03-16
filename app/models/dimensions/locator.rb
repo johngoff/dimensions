405c4fb583fdcf07be4d5da3d39967286ba3c228
Antonio Chavez <cavjzz@gmail.com>
2012-03-15 22:38:59 -0600

    Changed socialtags for categories

--- app/models/dimensions/locator.rb
+++ app/models/dimensions/locator.rb
@@ -4,4 +4,11 @@ class Dimensions::Locator
     serialized_data = geography.attributes.except("docId")
     entity = Entity.new(:name => geography.name, :serialized_data => serialized_data, :type => "location")
   end
+
+  def self.open_calails_tag(categories)
+    tags = {
+      0 => categories
+    }
+    entity = Entity.new(:name => geography.name, :serialized_data => tags, :type => "tags")
+  end
 end
